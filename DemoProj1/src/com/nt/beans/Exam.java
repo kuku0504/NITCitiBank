0a14455462e3354055817bc94600c34e5b49a6c1
developer2 <dev2@gmail.com>
2025-07-19 21:53:01 +0530

    welcome object is created by developer2

--- DemoProj1/src/com/nt/beans/Exam.java
+++ DemoProj1/src/com/nt/beans/Exam.java
@@ -4,5 +4,7 @@ public class Exam {
    public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("kuku");
-}
+	Welcome wel=new Welcome();
+	System.out.println("a new object is created of welcome class");
+	}
 }
